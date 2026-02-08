37ff959b199ea5f20786ab884f39494b0a9fe226
Rima <freebase-fame-keep@duck.com>
2026-02-08 13:57:00 -0800

    ebpf supports ARM64 now

--- internal/socket/ebpf/gen.go
+++ internal/socket/ebpf/gen.go
@@ -1,3 +1,3 @@
 package ebpf
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target native -cflags "-I/usr/include/x86_64-linux-gnu" Bpf bpf.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cflags "-I/usr/include/x86_64-linux-gnu" Bpf bpf.c
