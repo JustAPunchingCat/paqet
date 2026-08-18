7e515269a983a33ffcfd7f3edbc62d4ba88c5be9
JustAPunchingCat <freebase-fame-keep@duck.com>
2026-08-18 11:02:50 -0800

    fix(client/kcp): maintain active connection keepalive and extend smux timeout to 120s

--- internal/client/ticker.go
+++ internal/client/ticker.go
@@ -2,20 +2,36 @@ package client
 
 import (
 	"context"
+	"paqet/internal/tnet"
 	"time"
 )
 
 func (c *Client) ticker(ctx context.Context) {
-	timer := time.NewTimer(0)
-	defer timer.Stop()
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
 
 	for {
 		select {
-		case <-timer.C:
-			// for _, tc := range c.iter.Items {
-			// 	tc.sendTCPF(tc.conn)
-			// }
-			// timer.Reset(8 * time.Second)
+		case <-ticker.C:
+			for _, iter := range c.iters {
+				if iter == nil {
+					continue
+				}
+				for _, tc := range iter.Items {
+					if tc == nil {
+						continue
+					}
+					tc.mu.Lock()
+					conn := tc.conn
+					tc.mu.Unlock()
+
+					if conn != nil {
+						go func(cn tnet.Conn) {
+							_ = cn.Ping(false)
+						}(conn)
+					}
+				}
+			}
 		case <-ctx.Done():
 			return
 		}
